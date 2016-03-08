58c55fdf75e6fb3cb77a9e50ef11809436b89447
julisman <agung.julisman@yahoo.com>
2016-03-08 15:16:07 +0700

    change database to ralaliweb

--- auth-server/server/boot/routes.js
+++ auth-server/server/boot/routes.js
@@ -1,33 +1,52 @@
-module.exports = function(app) {
+/**
+ * author <agung.julisman@yahoo.com>
+ * overwrite route
+ */
 
+var _ = require('underscore');
+
+module.exports = function(app) {
+    /*load model user*/
     var User = app.models.RlUsers;
 
-    //login page
+    /*landing page*/
     app.get('/', function(req, res) {
         res.send({ status :  'api gateway is running' });
     });
 
-    //log a user in
+    /**
+     * Login a user by with the given `token`.
+     * require
+     *  -email
+     *  -password
+     */
     app.post('/auth', function(req, res) {
+        if(!_.isEmpty(req.body)){
+            User.login({
+                email: req.body.email,
+                password: req.body.password
+            }, 'user', function(err, token) {
+                if (err) {
+                    return res.status(401).send({ message: err.message });
+                }
+                res.send({ token:  token.id });
 
-        User.login({
-            email: req.body.email,
-            password: req.body.password
-        }, 'user', function(err, token) {
-            if (err) {
-                return res.status(401).send({ message: err.message });
-            }
-            res.send({ token:  token.id });
-
-        });
+            });
+        }else{
+            res.status(400).send({ message: 'body is empty' });
+        }
     });
 
-    //log a user out
+    /**
+     * Logout a user by header token.
+     * require
+     *  -token
+     */
     app.post('/logout', function(req, res, next) {
         if (!req.accessToken) return res.sendStatus(401);
         User.logout(req.accessToken.id, function(err) {
-            if (err) return next(err);
-            res.redirect('/');
+            if (err) return res.status(401).send({ message: err.message });
+            res.send();
         });
     });
 
