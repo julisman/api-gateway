d2b15f8c5e6d6d213cab0fd74b33c23a993659e9
julisman <agung.julisman@yahoo.com>
2016-03-10 10:22:58 +0700

    set status api gateway

--- auth-server/server/boot/routes.js
+++ auth-server/server/boot/routes.js
@@ -2,7 +2,10 @@
  * author <agung.julisman@yahoo.com>
  * overwrite route
  */
-module.exports = function(app) {
+module.exports = function(server) {
 
+    var router = server.loopback.Router();
+    router.get('/', server.loopback.status());
+    server.use(router);
 
 };
